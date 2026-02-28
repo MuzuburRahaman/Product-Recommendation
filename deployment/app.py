eed822f5e582f485f9bc88b1ff3d2707cf8b290b
MuzuburRahaman <muzuburrahaman1@gmail.com>
2026-03-01 02:05:14 +0530

    added port configs

--- deployment/app.py
+++ deployment/app.py
@@ -65,4 +65,5 @@ def get_recommendations(user_id):
 
 if __name__ == '__main__':
     print("Starting Flask server...")
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
